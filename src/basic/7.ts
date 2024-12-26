c3865b528758e729b1c9945b458e90fc9df00f6a
constantine1983 <constantineck83@gmail.com>
2024-12-26 06:01:41 +0200

    completed for checking

---
+++ src/basic/7.ts
@@ -0,0 +1,30 @@
+type Details = {
+  createAt: object;
+  updateAt: object;
+};
+
+type Account = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: Details;
+};
+
+const page1: Account = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date('2021-01-01'),
+    updateAt: new Date('2021-05-01'),
+  },
+};
+
+const page2: Account = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+};
